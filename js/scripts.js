d332305cecc038cc832394f496c57761792f3a3f
Krzysztof Ługowski <k.lugowski@yahoo.com>
2017-12-01 13:15:33 +0100

    Add new files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var withMenuItemsClass = document.getElementsByClassName('button');
+
+	for (var i = 0; i < 4; i++) {
+		var button = document.getElementsByTagName("button")[i].innerText;
+	
+	alert(button);
+}
+
+
